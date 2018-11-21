07d4769d082a45022d5143491f5a2ad5a2f13222
chenyq <1418806370@qq.com>
2018-11-21 10:20:55 +0800

    s

---
+++ src/test/java/cn/stylefeng/guns/system/TestMain.java
@@ -0,0 +1,13 @@
+package cn.stylefeng.guns.system;
+
+public class TestMain {
+    public int getCount(){
+        int a=1;
+        int b=2;
+        return a+b;
+    }
+    public static void main(String[] args) {
+        TestMain testMain = new TestMain();
+        testMain.getCount();
+    }
+}
